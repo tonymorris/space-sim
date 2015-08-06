3d0ba63fa3aa611f0e528558abb20f1f0fe54436
Tony Morris <tmorris@tmorris.net>
2015-08-06 11:52:28 +1000

    imports

--- src/Data/Space/Map.hs
+++ src/Data/Space/Map.hs
@@ -1,6 +1,6 @@
 module Data.Space.Map where
 
-import System.IO (readFile)
+
 import System.FilePath.Posix ((</>))
 import Prelude
 
@@ -9,18 +9,22 @@ data Flow a = Flow a a
 data SpaceMap a = SpaceMap [[Flow a]]
   deriving (Show)
 
+readTable :: String -> [[Double]]
 readTable = map (map read) . map words . lines
 
+readMap :: String -> String -> SpaceMap Double
 readMap x y = SpaceMap $ zipWith (zipWith Flow) tableX tableY
   where tableX = readTable x
         tableY = readTable y
 
-loadMapPart dir map kind c = readFile $ dir</>map++"_"++kind++c++".csv"
+loadMapPart :: FilePath -> [Char] -> [Char] -> [Char] -> IO String
+loadMapPart dir m kind c = readFile $ dir</>m++"_"++kind++c++".csv"
 
-loadFlow dir map = loadMapPart dir map "flow"
+loadFlow :: FilePath -> [Char] -> [Char] -> IO String
+loadFlow dir m = loadMapPart dir m "flow"
 
 loadMap :: FilePath -> String -> IO (SpaceMap Double)
-loadMap dir map = do
-  flowX <- loadFlow dir map "x"
-  flowY <- loadFlow dir map "y"
+loadMap dir m = do
+  flowX <- loadFlow dir m "x"
+  flowY <- loadFlow dir m "y"
   return $ readMap flowX flowY

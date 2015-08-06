d33e1318bc79465ad43cfb0f94c7b5cebd0af46e
Tony Morris <tmorris@tmorris.net>
2015-08-06 12:39:59 +1000

    lenses and things

--- src/Data/Space/Lobby.hs
+++ src/Data/Space/Lobby.hs
@@ -1,14 +1,26 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Data.Space.Lobby(
   lobby
 , LobbyResponse(..)
+, AsLobbyResponse(..)
+, AsName(..)
+, AsGame(..)
+, AsMap(..)
+, AsSecret(..)
 ) where
 
-import Prelude
-import GHC.Generics
-import Data.Typeable
-import Data.Aeson
+import Control.Category(Category(id))
+import Control.Lens
+import Data.Eq(Eq)
+import Data.Functor(Functor)
+import Data.String(String)
+import Data.Typeable(Typeable)
+import Data.Aeson(FromJSON)
+import GHC.Generics(Generic)
+import Prelude(Show)
 
 lobby ::
   String
@@ -24,3 +36,68 @@ data LobbyResponse =
   } deriving (Eq, Show, Generic, Typeable)
 
 instance FromJSON LobbyResponse
+
+class AsLobbyResponse p f s where
+  _LobbyResponse ::
+    Optic' p f s LobbyResponse
+
+instance AsLobbyResponse p f LobbyResponse where
+  _LobbyResponse =
+    id
+
+class AsName p f s where
+  _Name ::
+    Optic' p f s String
+
+instance AsName p f String where
+  _Name =
+    id
+
+instance Functor f => AsName (->) f LobbyResponse where
+  _Name =
+    lens
+      (\(LobbyResponse n _ _ _) -> n)
+      (\(LobbyResponse _ g m s) n -> LobbyResponse n g m s)
+
+class AsGame p f s where
+  _Game ::
+    Optic' p f s String
+
+instance AsGame p f String where
+  _Game =
+    id
+
+instance Functor f => AsGame (->) f LobbyResponse where
+  _Game =
+    lens
+      (\(LobbyResponse _ g _ _) -> g)
+      (\(LobbyResponse n _ m s) g -> LobbyResponse n g m s)
+
+class AsMap p f s where
+  _Map ::
+    Optic' p f s String
+
+instance AsMap p f String where
+  _Map =
+    id
+
+instance Functor f => AsMap (->) f LobbyResponse where
+  _Map =
+    lens
+      (\(LobbyResponse _ _ m _) -> m)
+      (\(LobbyResponse n g _ s) m -> LobbyResponse n g m s)
+      
+class AsSecret p f s where
+  _Secret ::
+    Optic' p f s String
+
+instance AsSecret p f String where
+  _Secret =
+    id
+
+instance Functor f => AsSecret (->) f LobbyResponse where
+  _Secret =
+    lens
+      (\(LobbyResponse _ _ _ s) -> s)
+      (\(LobbyResponse n g m _) s -> LobbyResponse n g m s)
+      

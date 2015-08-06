318a8cf6948c8bd05787df8b92bf26f4090f6b21
Kenni Bawden <kenni.bawden@gmail.com>
2015-08-06 11:01:42 +1000

    Added map data type with loading functions

---
+++ src/Data/SpaceMap.hs
@@ -0,0 +1,24 @@
+module Data.SpaceMap where
+
+import System.IO (readFile)
+
+data Flow a = Flow a a
+  deriving (Show)
+data SpaceMap a = SpaceMap [[Flow a]]
+  deriving (Show)
+
+readTable = map (map read) . map words . lines
+
+readMap x y = SpaceMap $ zipWith (zipWith Flow) tableX tableY
+  where tableX = readTable x
+        tableY = readTable y
+
+loadMapPart dir map kind c = readFile $ dir++map++"_"++kind++c++".csv"
+
+loadFlow dir map = loadMapPart dir map "flow"
+
+loadMap :: FilePath -> FilePath -> IO (SpaceMap Double)
+loadMap dir map = do
+  flowX <- loadFlow dir map "x"
+  flowY <- loadFlow dir map "y"
+  return $ readMap flowX flowY
